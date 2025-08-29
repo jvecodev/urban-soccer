93f2f170bd708d163cd747aafb8689ffcb10ac00
jvecodev <jvitor.oliveira1803@gmail.com>
2025-08-29 16:09:18 -0300

    chore(PrimeNG): instalando prime Ng no projeto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/lara';
 
 import { routes } from './app.routes';
 
@@ -7,6 +9,11 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Lara
+      }
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
