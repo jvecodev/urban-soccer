a0c5a7b50d9bd11bfb39204b41cc275c7788c666
João Vitor <jvecodev@users.noreply.github.com>
2025-10-02 11:04:27 -0300

    fix(environment): corrigir URL da API para produção

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',
+  // apiUrl: 'http://localhost:8000',
+  apiUrl: 'https://urbansoccer-api.onrender.com',
   apiTimeout: 10000,
 };
