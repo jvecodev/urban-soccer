5c138be689f6c86fa0a8d8e755a163a259e4bf70
João Vitor <jvecodev@users.noreply.github.com>
2025-10-02 21:11:54 -0300

    fix(game-start): remover seção de título e ajustar layout para responsividade

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8000',
-  apiUrl: 'https://urbansoccer-server.onrender.com',
+  apiUrl: 'http://localhost:8000',
+  // apiUrl: 'https://urbansoccer-server.onrender.com',
   apiTimeout: 10000,
 };
